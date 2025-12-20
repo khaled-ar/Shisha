d3be0251b035bbbc3908b0b310a386a56d556b38
khaled-ar <khaled.ar.work@gmail.com>
2025-12-19 18:02:59 -0800

    changes

--- routes/employee_products_routes.php
+++ routes/employee_products_routes.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('employee_products')->group(function() {
+    Route::get('employees/sections/products/categories', [CategoriesController::class, 'index'])
+        ->name('employee.categories');
+
     Route::apiResource('employees/sections/products', ProductsController::class)
         ->names('employee.products');
 });
