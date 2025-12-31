fd2490f8044a4f92907876b6a7a5fd247ed4e567
khaled-ar <khaled.ar.work@gmail.com>
2025-12-30 17:09:51 -0800

    changes

--- app/Http/Controllers/Admin/ProductsOrdersController.php
+++ app/Http/Controllers/Admin/ProductsOrdersController.php
@@ -15,8 +15,8 @@ class ProductsOrdersController extends Controller
     public function index()
     {
         $status = request('status');
+        $store = Store::first();
         if(request('driver_id')) {
-            $store = Store::first();
             $products_orders = ProductsOrder::whereStatus($status)
                 ->whereEmployeeId(request('driver_id'))
                 ->whereHas('user')
@@ -57,10 +57,49 @@ class ProductsOrdersController extends Controller
                 ->values();
 
             return $this->generalResponse($products_orders);
-        } else { // User Orders Case
-            $store = Store::first();
+        } elseif(request('driver_id')) {
             $products_orders = ProductsOrder::whereStatus($status)
                 ->whereUserId(request('user_id'))
+                ->whereHas('user')
+                ->whereHas('product')
+                ->with(['user', 'product'])
+                ->latest()
+                ->get()
+                ->groupBy('user_id')
+                ->map(function ($userOrders) use($store){
+                    $firstOrder = $userOrders->first();
+                    $user = $firstOrder->user;
+
+                    $totalSum = $userOrders->sum('total');
+
+                    return [
+                        'user_id' => $user->id,
+                        'username' => $user->name,
+                        'image_url' => $user->image_url,
+                        'phone' => $user->phone,
+                        'total' => $totalSum,
+                        'delivery_cost' => $firstOrder->delivery_cost,
+                        'destination_lon' => $firstOrder->lon,
+                        'destination_lat' => $firstOrder->lat,
+                        'store_lon' => $store->lon,
+                        'store_lat' => $store->lat,
+                        'products' => $userOrders->map(function($order) {
+                            return [
+                                'order_id' => $order->id,
+                                'created_at' => $order->created_at,
+                                'product_title' => $order->product->title,
+                                'product_images' => $order->product->images_urls,
+                                'product_price' => $order->product->price,
+                                'quantity' => $order->quantity,
+                            ];
+                        }),
+                    ];
+                })
+                ->values();
+
+            return $this->generalResponse($products_orders);
+        } else {
+                $products_orders = ProductsOrder::whereStatus($status)
                 ->whereHas('user')
                 ->whereHas('product')
                 ->with(['user', 'product'])
